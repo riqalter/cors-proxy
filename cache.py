8ce9d47b08d09e7ee246aed1fd6440fd31ab5461
riqalter <riqalter@gmail.com>
2025-06-13 21:40:18 +0700

    make cache
    Signed-off-by: riqalter <riqalter@gmail.com>

--- cache.py
+++ cache.py
@@ -1,1 +1,15 @@
+import redis
+from config import REDIS_URL
+import hashlib
+
+r: redis.Redis = redis.Redis.from_url(REDIS_URL)
+
+def cache_key(url: str) -> str:
+    return "cache:" + hashlib.sha256(url.encode()).hexdigest()
+
+def get_cached_response(url: str) -> bytes | None:
+    return r.get(cache_key(url)) # type: ignore
+
+def set_cached_response(url: str, content: bytes, ttl: int = 60) -> None:
+    r.setex(cache_key(url), ttl, content)
 
